597cf7cfc981727d4cc93ed51ac2952472b413d9
João Vitor Lima <jvolima2004@gmail.com>
2022-09-05 19:55:29 -0300

    Tipando useSelector

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,9 +1,9 @@
 import { useSelector } from "react-redux"
+import { IState } from "../store";
+import { ICartItem } from "../store/modules/cart/types";
 
 export function Cart() {
-  const state = useSelector(state => state);
-
-  console.log(state)
+  const cart = useSelector<IState, ICartItem[]>(state => state.cart.items);
 
   return (
     <table>
@@ -15,6 +15,16 @@ export function Cart() {
           <th>Subtotal</th>
         </tr>
       </thead>
+      <tbody>
+        {cart.map(item => (
+          <tr key={item.product.id}>
+            <td>{item.product.title}</td>
+            <td>{item.product.price}</td>
+            <td>{item.quantity}</td>
+            <td>{(item.product.price * item.quantity).toFixed(2)}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   )
 }
